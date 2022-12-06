fd384fb8f2d73e14e9fcce6aef68eaee531ff579
Camilo Gomez <juancamilo080253@gmail.com>
2022-12-05 23:10:44 -0500

    se agrego  conector base de datos, controlador antes de solicitar datos de la bd

--- src/main/java/com/trabajo/crud/plataformas/controladorInicio.java
+++ src/main/java/com/trabajo/crud/plataformas/controladorInicio.java
@@ -1,14 +1,47 @@
 package com.trabajo.crud.plataformas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.trabajo.crud.plataformas.dominio.persona;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Controller
+@Slf4j
 
 public class controladorInicio {
-    
+
     @GetMapping("/")
-    public String inicio(){
-        return "Hola amor mio corazon de otro";
+    public String inicio(Model model){
+        var mensaje="ADIOS CON THYMELEAF";
+
+        var persona = new persona();
+        persona.setNombre("Camilo");
+        persona.setApellido("Gomez");
+        persona.setTelefono("3012844505");
+        persona.setEmail("juan@hot.com");
+
+        var persona2 = new persona();
+        persona2.setNombre("Karla");
+        persona2.setApellido("Gomez");
+        persona2.setTelefono("30125445505");
+        persona2.setEmail("karla23@hot.com");
+
+        // var personas = new ArrayList<>();
+        // personas.add(persona);
+        // personas.add(persona2);
+
+        var personas = Arrays.asList(persona,persona2);
+
+        log.info("EJECUTANDO EL CONTROLADOR MVC");
+        model.addAttribute("mensaje", mensaje);
+        //model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
+        return "index";
     }
 }
---
+++ src/main/java/com/trabajo/crud/plataformas/dominio/persona.java
@@ -0,0 +1,12 @@
+package com.trabajo.crud.plataformas.dominio;
+
+import lombok.Data;
+
+@Data
+public class persona {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    
+}
